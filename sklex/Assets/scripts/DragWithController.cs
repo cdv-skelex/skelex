b5722e336e9551c8f0952cf0cc2c4e0635e9169b
Sebastian Schulz <sebastian.schulz@student.hpi.de>
2017-10-26 18:47:04 +0200

    add dissected model, add demo with draggable part
    Former-commit-id: 83d6ffb60c3292d876b49dad32d4496499a0cb3d

---
+++ sklex/Assets/scripts/DragWithController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragWithController : MonoBehaviour {
+
+    private SteamVR_TrackedController _controller;
+    public GameObject DraggableGameObject;
+
+    private void OnEnable()
+    {
+        _controller = GetComponent<SteamVR_TrackedController>();
+        _controller.TriggerClicked += HandleTriggerClicked;
+        _controller.TriggerUnclicked += HandleTriggerUnclicked;
+    }
+
+    private void HandleTriggerClicked(object sender, ClickedEventArgs e)
+    {
+        DraggableGameObject.transform.parent = gameObject.transform;
+    }
+
+    private void HandleTriggerUnclicked(object sender, ClickedEventArgs e)
+    {
+        DraggableGameObject.transform.parent = null;
+    }
+}
