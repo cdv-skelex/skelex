67a5eba656e80fe2a51c4427ec5e93844f2fb741
Sebastian Schulz <sebastian.schulz@student.hpi.de>
2017-10-27 19:11:20 +0200

    add simple collision detection demo
    Former-commit-id: 094fb57a0f5f6eac087472abbce2b974ed77bb47

---
+++ sklex/Assets/scripts/CollisionDetection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    public Material Material;
+
+	// Use this for initialization
+	void Start () {
+	    //Material.shader = Shader.Find("Specular");
+	    Material.SetColor("_SpecColor", Color.white);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        Material.SetColor("_SpecColor", Color.green);
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        Material.SetColor("_SpecColor", Color.white);
+    }
+}
