f07b9c4c34de5e1fcb079289a107d9dabfe1dfc7
Sebastian Schulz <sebastian.schulz@student.hpi.de>
2017-11-10 21:19:49 +0100

    add linaeaiael
    Former-commit-id: 8a4dd074d7f5d9ceae0b837c54b6956b74a1e4f9

---
+++ sklex/Assets/scripts/MeasureTape.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeasureTape : MonoBehaviour
+{
+    public GameObject ControllerLeft;
+    public GameObject ControllerRight;
+
+    public GameObject TapeContainer;
+    public GameObject Text;
+    private TextMesh _textMesh;
+    private Material _material;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _textMesh = Text.GetComponent<TextMesh>();
+	    Text.transform.parent = ControllerLeft.transform;
+	    _material = GetComponent<MeshRenderer>().material;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var left = ControllerLeft.transform.position;
+	    var right = ControllerRight.transform.position;
+        TapeContainer.transform.position = (left + right) / 2;
+	    var rotation = Quaternion.LookRotation(right - left);
+	    gameObject.transform.rotation = rotation;
+	    var scale = Vector3.Distance(left, right);
+	    gameObject.transform.localScale = new Vector3(0.0001f, 0.02f, scale);
+
+	    _textMesh.text = scale.ToString();
+
+        _material.mainTextureScale = new Vector2(scale * 10, 1);
+	}
+}
