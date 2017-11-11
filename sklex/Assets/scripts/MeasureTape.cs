2fac2f0993fe7154bbdf78187292d0bf99e609ac
Sebastian Schulz <sebastian.schulz@student.hpi.de>
2017-11-11 11:14:49 +0100

    only show tape when pressing touch pad
    Former-commit-id: 8831c743043cd683187c457de14f8e411ac0627b

--- sklex/Assets/scripts/MeasureTape.cs
+++ sklex/Assets/scripts/MeasureTape.cs
@@ -14,18 +14,26 @@ public class MeasureTape : MonoBehaviour
     public GameObject TapeContainer;
     public GameObject Text;
     private TextMesh _textMesh;
+
+    private MeshRenderer _meshRenderer;
     private Material _material;
+
 
 	// Use this for initialization
 	void Start ()
 	{
 	    _textMesh = Text.GetComponent<TextMesh>();
 	    Text.transform.parent = ControllerLeft.transform;
-	    _material = GetComponent<MeshRenderer>().material;
+
+	    _meshRenderer = GetComponent<MeshRenderer>();
+	    _material = _meshRenderer.material;
+
 	    _trackedLeftController = ControllerLeft.GetComponent<SteamVR_TrackedController>();
 
-	    _trackedLeftController.PadClicked += (sender, args) => { enabled = true; };
-	    _trackedLeftController.PadUnclicked += (sender, args) => { enabled = false; };
+	    _meshRenderer.enabled = false;
+	    _textMesh.text = "";
+	    _trackedLeftController.PadClicked += (sender, args) => { _meshRenderer.enabled = true; };
+	    _trackedLeftController.PadUnclicked += (sender, args) => { _meshRenderer.enabled = false; };
 	}
 
     // Update is called once per frame
@@ -40,7 +48,7 @@ public class MeasureTape : MonoBehaviour
 	    gameObject.transform.localScale = new Vector3(0.0001f, 0.02f, scale);
 
 	    var s = (scale / (10 * Skeleton.transform.localScale.x)).ToString();
-        _textMesh.text = s.Substring(0, Math.Min(4, s.Length)) + "cm";
+        _textMesh.text = _trackedLeftController.padPressed ? s.Substring(0, Math.Min(4, s.Length)) + "cm" : "";
 
         _material.mainTextureScale = new Vector2(scale * 10, 1);
 	}
