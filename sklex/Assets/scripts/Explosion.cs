2108f667c04937a3b76a125354177fc300d7d312
Sebastian Schulz <sebastian.schulz@student.hpi.de>
2017-11-11 13:31:17 +0100

    add explosion script
    Former-commit-id: 041f23e4c6efa4349e2aa5a05ec9d2d0ffcddf84

---
+++ sklex/Assets/scripts/Explosion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    private Vector3 _center;
+    private GameObject _model;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    var min = new Vector3();
+	    var max = new Vector3();
+
+	    foreach (var meshRenderer in GetComponentsInChildren<MeshRenderer>())
+	    {
+	        min = Vector3.Min(min, meshRenderer.bounds.min);
+	        max = Vector3.Max(max, meshRenderer.bounds.max);
+	    }
+
+	    _model = gameObject.transform.parent.gameObject;
+
+	    _center = ((min + max) / 2) - (_model.transform.position * 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        if (Time.time < 2)
+	        transform.position = transform.position + _center * Time.deltaTime;
+
+        if (Time.time > 5 && Time.time < 7)
+            transform.position = transform.position - _center * Time.deltaTime;
+    }
+}
