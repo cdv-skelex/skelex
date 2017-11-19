4a32549b5b786ce5d9e8862ee8dfd44f75d3273d
Sebastian Schulz <sebastian.schulz@student.hpi.de>
2017-11-19 13:51:24 +0100

    mouse stuff
    Former-commit-id: ee791051564b640145d4e2ea6ed9cc22dd9d6d5f

--- sklex/Assets/scripts/BiteAnimation.cs
+++ sklex/Assets/scripts/BiteAnimation.cs
@@ -1,35 +1,76 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BiteAnimation : MonoBehaviour
 {
-    public GameObject[] Top;
-    public GameObject[] Bottom;
-
     public GameObject TopGroup;
     public GameObject BottomGroup;
 
-	// Use this for initialization
+    public SteamVR_TrackedController Controller;
+
+    public GameObject Mouse;
+
+    public GameObject[] Top;
+    public GameObject[] Bottom;
+
+    private bool _active;
+
+    // Use this for initialization
 	void Start () {
-	    foreach (var bone in Top)
+	    Controller.PadClicked += (sender, args) =>
 	    {
-	        bone.transform.parent = TopGroup.transform;
-	    }
-	    foreach (var bone in Bottom)
-	    {
-	        bone.transform.parent = BottomGroup.transform;
-	    }
+	        if (-args.padY > Math.Abs(args.padX) && args.padY < 0f)
+	            ToggleAnimation();
+	    };
     }
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	    /*var t = Mathf.Min(Time.time, 0.35f);
+	}
+
+    void ToggleAnimation()
+    {
+        _active = !_active;
+
+        Mouse.GetComponent<MeshRenderer>().enabled = _active;
+
+        if (_active)
+            StartAnimation();
+        else
+            StopAnimation();
+    }
+
+    void StartAnimation()
+    {
+        TopGroup.transform.rotation = transform.rotation;
+        BottomGroup.transform.rotation = transform.rotation;
+
         foreach (var bone in Top)
         {
-            bone.transform.position = transform.position + new Vector3(0f, t, 0f) * transform.localScale.x;
-            bone.transform.rotation = Quaternion.Euler(t, 0f, 0f);
-        }*/
-	}
+            bone.transform.parent = TopGroup.transform;
+        }
+        foreach (var bone in Bottom)
+        {
+            bone.transform.parent = BottomGroup.transform;
+        }
+    }
+
+    void StopAnimation()
+    {
+        foreach (var bone in Top)
+        {
+            bone.transform.parent = transform;
+            bone.transform.position = transform.position;
+            bone.transform.rotation = transform.rotation * Quaternion.Euler(-90f, 0f, 0f);
+        }
+        foreach (var bone in Bottom)
+        {
+            bone.transform.parent = transform;
+            bone.transform.position = transform.position;
+            bone.transform.rotation = transform.rotation * Quaternion.Euler(-90f, 0f, 0f);
+        }
+    }
 }
