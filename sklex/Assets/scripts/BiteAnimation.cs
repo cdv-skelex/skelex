b2d4ae9e7644731a537b242fa8123cf6ec89d07e
Sebastian Schulz <sebastian.schulz@student.hpi.de>
2017-11-19 11:26:18 +0100

    some changes
    Former-commit-id: fbe1cd84200968bed1a2a25b4cd830ecabba2623

---
+++ sklex/Assets/scripts/BiteAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiteAnimation : MonoBehaviour
+{
+    public GameObject[] Top;
+    public GameObject[] Bottom;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (Time.time < 5)
+        {
+            foreach (var bone in Top)
+            {
+                bone.transform.position = transform.position + new Vector3(0f, Time.deltaTime, 0f);
+                bone.transform.rotation *= Quaternion.Euler(1f * Time.deltaTime, 0f, 0f);
+            }
+        }
+	}
+}
