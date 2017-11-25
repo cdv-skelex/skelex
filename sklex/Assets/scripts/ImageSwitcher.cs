982e43766ae6fafa97699d21bb42d9f6e8355178
Sebastian Schulz <sebastian.schulz95@gmail.com>
2017-11-25 01:32:14 +0100

    add labels for models, add switching for wiki infos
    Former-commit-id: f2357ae6446d5248a86e75c055040e248d780842

---
+++ sklex/Assets/scripts/ImageSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageSwitcher : MonoBehaviour {
+
+	public Material Mat;
+	public Texture[] Textures;
+	private int Index;
+
+	public SteamVR_TrackedController Controller;
+
+	// Use this for initialization
+	void Start () {
+		Controller.MenuButtonClicked += (sender, args) => {
+			Next()
+		};
+	}
+	
+	void Next() {
+		Index = (Index+1)%Textures.Length;
+		Mat.mainTexture = Textures[Index];
+	}
+}
---
+++ sklex/Assets/scripts/ModelLabel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelLabel : MonoBehaviour {
+	public GameObject HMD;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation =
+	            Quaternion.LookRotation(transform.position - HMD.transform.position);
+	}
+}
